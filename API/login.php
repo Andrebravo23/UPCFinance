2d81fc32209b6a6c6de34592e7f6cfdb697b84f5
Andrebravo23 <hbravo798@gmail.com>
2022-11-18 18:35:35 -0500

    Se añadieron con éxito

---
+++ API/login.php
@@ -0,0 +1,88 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method) {
+    case "POST":
+        if ($_GET['action'] == 'register')
+        {
+            $email = $_POST['email-register'];
+            $name = $_POST['name'];
+            $lastName = $_POST['last-name'];
+            $password = password_hash($_POST['password-register'], 1);
+
+            $sql = "INSERT INTO usuario (nombres, apellidos, correo, contrasena)
+                    VALUES ('$name', '$lastName', '$email', '$password')";
+            $stmt = $conn->prepare($sql);
+            try {
+                $result = $stmt->execute();
+                echo json_encode([
+                    'result' => 1,
+                    'message' => 'Registraste tu cuenta'
+                ]);
+            } catch (\Throwable $th) {
+                $sql_error = $stmt->errorInfo()[1];
+                if ($sql_error == 1062) {
+                    echo json_encode([
+                        'result' => 0,
+                        'message' => 'El email ingresado ya fue registrado'
+                    ]);
+                } else {
+                    echo json_encode([
+                        'result' => 0,
+                        'message' => 'Ha habido un error al intentar registrarte'
+                    ]);
+                }
+            }
+        }
+        else
+        {
+            session_start();
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            
+            $sql = "SELECT * FROM usuario WHERE correo = '$email'";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if(!$usuario) {
+                echo json_encode([
+                    'result' => 0,
+                    'message' => 'Usuario no encontrado'
+                ]);
+                die;
+            }
+
+            if (password_verify($password, $usuario['contrasena'])) {
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['name'] = $_POST['email'];
+                $_SESSION['id'] = $usuario['id'];
+                
+                echo json_encode([
+                    'result' => 1,
+                    'message' => 'Sesión iniciada'
+                ]);
+
+                header('Location: ../test.php');
+            } else {
+                echo json_encode([
+                    'result' => 0,
+                    'message' => 'Contraseña incorrecta'
+                ]);
+            }
+        }
+        break;
+    default:
+        exit('Method not supported');
+        break;
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,90 @@
-function showRegister() {
-    $('#log-in').fadeOut();
+const nots = $('#nots');
+const notsIcon = $('#nots i');
+const notsText = $('#nots span');
+
+const notsIcons = {
+    'success': 'bi bi-check-circle',
+    'fail': 'bi bi-x-circle'
 }
+
+let notsTimeout = undefined;
+
+function showNots(type, text) {
+    clearTimeout(notsTimeout);
+    nots.fadeIn('fast');
+    nots.attr('class', '');
+    nots.addClass(type);
+    notsIcon.attr('class', '');
+    notsIcon.addClass(notsIcons[type]);
+    notsText.html(text);
+    notsTimeout = setTimeout(() => {
+        nots.fadeOut('fast');
+    }, 3000);
+}
+
+let onRegister = false;
+
+function toggleRegister(e) {
+    if (!onRegister) {
+        $('#log-in').fadeOut("fast", function() {
+            $('#register').fadeIn("fast");
+            onRegister = true;
+        });
+    } else {
+        $('#register').fadeOut("fast", function() {
+            $('#log-in').fadeIn("fast");
+            onRegister = false;
+        });
+    }
+}
+
+$('#register').on('submit', function(e) {
+    let _form = $(this);
+
+    e.preventDefault();
+    if ($('#password-register').val() != $('#repeat-password').val()) {
+        showNots('fail', 'Las contraseñas no son iguales');
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "./API/login.php?action=register",
+        data: _form.serialize(),
+        success: function (response) {
+            response = JSON.parse(response);
+            if (response.result == 1) {
+                toggleRegister();
+                _form.trigger("reset");
+                showNots('success', response.message);
+            } else {
+                showNots('fail', response.message);
+            }
+        },
+        error: function (response) {
+            showNots('fail', 'Ha ocurrido un error');
+        }
+    });
+});
+
+$('#log-in').on('submit', function(e) {
+    let _form = $(this);
+
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "./API/login.php?action=login",
+        data: _form.serialize(),
+        success: function (response) {
+            response = JSON.parse(response);
+            if (response.result == 1) {
+                
+            } else {
+                showNots('fail', response.message);
+            }
+        },
+        error: function (response) {
+            showNots('fail', 'Ha ocurrido un error');
+        }
+    });
+});
